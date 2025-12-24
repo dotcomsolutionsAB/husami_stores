abf49a8ce4ea9f01960a717143b4bf12cd57b544
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-12-24 16:38:01 +0530

    update

--- app/Models/CounterModel.php
+++ app/Models/CounterModel.php
@@ -9,4 +9,19 @@ class CounterModel extends Model
     //
     protected $table = 't_counter';
     protected $fillable = ['name', 'prefix', 'number', 'postfix'];
+
+    // so it auto-appears in JSON responses
+    protected $appends = ['formatted'];
+
+    public function getFormattedAttribute(): string
+    {
+        $num = (string)($this->number ?? 0);
+
+        // pad to 4 only if < 4 digits
+        if (strlen($num) < 4) {
+            $num = str_pad($num, 4, '0', STR_PAD_LEFT);
+        }
+
+        return (string)($this->prefix ?? '') . $num . (string)($this->postfix ?? '');
+    }
 }
