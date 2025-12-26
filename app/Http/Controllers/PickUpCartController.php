f004dfc2f9c80126743870fbb8429ab658b002a7
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-12-26 16:55:27 +0530

    fix

--- app/Http/Controllers/PickUpCartController.php
+++ app/Http/Controllers/PickUpCartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Traits\ApiResponse;
 use App\Models\PickUpCartModel;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
