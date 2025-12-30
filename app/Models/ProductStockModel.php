11794462747e23e537432b0166960c08529b9f2b
devbygongo <gsample7894.sg6@gmail.com>
2025-12-30 18:59:20 +0530

    fix

--- app/Models/ProductStockModel.php
+++ app/Models/ProductStockModel.php
@@ -9,7 +9,7 @@ class ProductStockModel extends Model
     //
     protected $table = 't_product_stocks';
     // protected $fillable = []; // Extend later if you add fields like product_id, qty, etc.
-    protected $fillable = ['product_id', 'godown_id', 'quantity', 'ctn', 'sent', 'batch_no', 'rack_no', 'invoice_no', 'invoice_date', 'tc_no', 'tc_date', 'tc_attachment', 'remarks']; // Extend later if you add fields like product_id, qty, etc.
+    protected $fillable = ['sku', 'godown_id', 'quantity', 'ctn', 'sent', 'batch_no', 'rack_no', 'invoice_no', 'invoice_date', 'tc_no', 'tc_date', 'tc_attachment', 'remarks']; // Extend later if you add fields like product_id, qty, etc.
 
     protected $casts = [
         'invoice_date' => 'date',
